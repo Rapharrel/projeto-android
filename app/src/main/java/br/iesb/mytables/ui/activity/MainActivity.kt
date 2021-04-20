a02e6d6e577e969bffad9f2a8b0590daaef0fbce
Rapharrel <rafaelbarbosa525@gmail.com>
2021-04-19 23:21:15 -0300

    Create Project

---
+++ app/src/main/java/br/iesb/mytables/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.iesb.mytables.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.iesb.mytables.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
